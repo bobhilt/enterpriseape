cee4a3c15d0a4fc6cae6e209461dc89ddd3138f1
Bob Hiltner <bobhilt@gmail.com>
2015-04-08 11:46:39 -0700

    db employee list plus adding figaro

---
+++ db/migrate/20150407202804_remove_saleperson_from_invoices.rb
@@ -0,0 +1,6 @@
+class RemoveSalepersonFromInvoices < ActiveRecord::Migration
+  def change
+    remove_column :invoices, :salesperson, :string
+    add_column :invoices, :employee_id, :integer
+  end
+end
